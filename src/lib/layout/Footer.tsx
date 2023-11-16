5ad8d9a01e9d916b0c198dc160b7d588bb5acba1
Samuel Santos <csamuelssm@gmail.com>
2023-11-16 08:38:10 -0300

    i think now the ads are working

--- src/lib/layout/Footer.tsx
+++ src/lib/layout/Footer.tsx
@@ -1,5 +1,5 @@
 import { Flex, Link, Text, Button } from '@chakra-ui/react';
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useEffect, useRef } from 'react';
 import { FaHandHoldingHeart } from 'react-icons/fa';
 import { track } from '@vercel/analytics';
 import Script from 'next/script';
@@ -9,6 +9,29 @@ type FooterProps = {
 }
 
 const Footer = ( props:FooterProps ) => {
+
+  const banner = useRef<HTMLDivElement>(null);
+  const atOptions = {
+    key: '7d2c5adb840e04096ed73658ccba6760',
+    format: 'iframe',
+    height: 60,
+    width: 468,
+    params: {}
+  }
+
+  useEffect(() => {
+    if (banner.current && !banner.current.firstChild) {
+      const conf = document.createElement('script');
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = `//www.highperformancedformats.com/${atOptions.key}/invoke.js`;
+      conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`;
+
+      banner.current.append(conf);
+      banner.current.append(script);
+    }
+  }, [banner]) 
+
   return (
     <Flex as="footer" width="full" justifyContent="center" alignItems='center' display='flex' flexDirection='column' textAlign='center'>
       <Text fontSize="sm">
@@ -17,22 +40,11 @@ const Footer = ( props:FooterProps ) => {
           samuel santos
         </Link>
       </Text>
-      <Flex w='468px' h='60px' marginY={1}>
-        <Script>
-          {
-            `
-            atOptions = {
-              'key' : '7d2c5adb840e04096ed73658ccba6760',
-              'format' : 'iframe',
-              'height' : 60,
-              'width' : 468,
-              'params' : {}
-            };
-            document.write('<scr' + 'ipt type="text/javascript" src="//www.highcpmcreativeformat.com/7d2c5adb840e04096ed73658ccba6760/invoke.js"></scr' + 'ipt>');
-            `
-          }
-        </Script>
-      </Flex>
+      <div ref={banner} style={{
+        marginTop: '3px',
+        marginBottom: '8px'
+      }}>
+      </div>
       <Button display='block' width='auto' colorScheme='green' variant='outline' rightIcon={<FaHandHoldingHeart />}
           onClick={() => {
               track('Donation')
