907e127819e3d1f70deafa701684cdb8455e3de0
Samuel Santos <csamuelssm@gmail.com>
2023-11-17 14:46:06 -0300

    adding tooltips to buttons

--- src/lib/layout/Header.tsx
+++ src/lib/layout/Header.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, IconButton, Menu, MenuButton, MenuList, MenuItem, Link } from '@chakra-ui/react';
+import { Box, Flex, Heading, IconButton, Menu, MenuButton, MenuList, MenuItem, Link, Tooltip } from '@chakra-ui/react';
 import { FaLanguage, FaHandHoldingHeart, FaBars, FaRegCommentAlt, FaRegFlag, FaRegLightbulb, FaRegCalendarAlt } from 'react-icons/fa';
 import { MdMovieFilter } from 'react-icons/md';
 
@@ -32,14 +32,16 @@ const Header = (props:HeaderProps) => {
       </Box>
       <Flex flexDirection="row" alignItems="flex-end" justifyContent="flex-end" w="100%">
         <Box marginX={1.5}>
-          <IconButton
-            aria-label="give up"
-            icon={<FaRegFlag />}
-            isDisabled={props.canGiveUp}
-            onClick={() => {
-              setGiveUp(true);
-            }}
-          />
+          <Tooltip hasArrow label='Desistir'>
+            <IconButton
+              aria-label="give up"
+              icon={<FaRegFlag />}
+              isDisabled={props.canGiveUp}
+              onClick={() => {
+                setGiveUp(true);
+              }}
+            />
+          </Tooltip>
         </Box>
         <Box marginX={1.5}>
           {/*<IconButton
@@ -49,30 +51,32 @@ const Header = (props:HeaderProps) => {
               props.setDonation(true);
             }}
           />*/}
-          <Menu>
-            <MenuButton as={IconButton} icon={<FaBars />} />
-            <MenuList>
-              <MenuItem icon={<FaRegCalendarAlt/>} onClick={() => {
-                props.setOldGames(true);
-              }}>
-                Jogos antigos
-              </MenuItem>
-              <MenuItem icon={<FaHandHoldingHeart/>} onClick={() => {
-                props.setDonation(true);
-              }}>Doar</MenuItem>
-              <MenuItem as='a' href='https://forms.gle/32FbMft1d9hiVep49' icon={<FaRegCommentAlt/>}>
-                Feedback
-              </MenuItem>
-              <MenuItem icon={<FaRegLightbulb/>} onClick={() => {
-                props.setCredits(true);
-              }}>
-                Créditos
-              </MenuItem>
-              <MenuItem as='a' href='https://guess-the-movie-one.vercel.app/' icon={<FaLanguage />}>
-                Versão inglês
-              </MenuItem>
-            </MenuList>
-          </Menu>
+            <Menu>
+              <Tooltip hasArrow label='Menu'>
+                <MenuButton as={IconButton} icon={<FaBars />} />
+              </Tooltip>
+              <MenuList>
+                <MenuItem icon={<FaRegCalendarAlt/>} onClick={() => {
+                  props.setOldGames(true);
+                }}>
+                  Jogos antigos
+                </MenuItem>
+                <MenuItem icon={<FaHandHoldingHeart/>} onClick={() => {
+                  props.setDonation(true);
+                }}>Doar</MenuItem>
+                <MenuItem as='a' href='https://forms.gle/32FbMft1d9hiVep49' icon={<FaRegCommentAlt/>}>
+                  Feedback
+                </MenuItem>
+                <MenuItem icon={<FaRegLightbulb/>} onClick={() => {
+                  props.setCredits(true);
+                }}>
+                  Créditos
+                </MenuItem>
+                <MenuItem as='a' href='https://guess-the-movie-one.vercel.app/' icon={<FaLanguage />}>
+                  Versão inglês
+                </MenuItem>
+              </MenuList>
+            </Menu>
         </Box>
         <Box marginX={1.5}>
           <ThemeToggle />
--- src/lib/layout/ThemeToggle.tsx
+++ src/lib/layout/ThemeToggle.tsx
@@ -1,15 +1,17 @@
-import { IconButton, useColorMode } from '@chakra-ui/react';
+import { IconButton, Tooltip, useColorMode } from '@chakra-ui/react';
 import { RiMoonFill, RiSunLine } from 'react-icons/ri';
 
 const ThemeToggle = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <IconButton
-      aria-label="theme toggle"
-      icon={colorMode === 'light' ? <RiMoonFill /> : <RiSunLine />}
-      onClick={toggleColorMode}
-    />
+    <Tooltip hasArrow label='Mudar tema'>
+      <IconButton
+        aria-label="theme toggle"
+        icon={colorMode === 'light' ? <RiMoonFill /> : <RiSunLine />}
+        onClick={toggleColorMode}
+      />
+    </Tooltip>
   );
 };
 
