7a77811e74cafe557e64d3b61004ff752ee81808
Samuel Santos <csamuelssm@gmail.com>
2024-10-31 11:09:07 -0300

    changing image component to standard img component

--- src/lib/components/PixModal.tsx
+++ src/lib/components/PixModal.tsx
@@ -1,6 +1,7 @@
 import React, { Dispatch, SetStateAction } from 'react'
-import { Image, Text, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Button, Flex, Link } from '@chakra-ui/react'
+import { Text, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Button, Flex, Link } from '@chakra-ui/react'
 //import QRCodePix from '../../../public/qr-code-pix.png';
+//import Image from 'next/image'
 
 type ModalProps = {
     open: boolean,
@@ -21,7 +22,10 @@ function PixModal(props:ModalProps) {
                     Você pode scanear este QR Code e fazer um pix para Cicero Samuel Santos Morais.
                 </Text>
                 <Flex w='100%' textAlign='center' alignItems='center' flexDirection='column'>
-                    <Image src='https://ibb.co/tJMfstJ' alt='qr-code pix' marginY={3} />
+                    <img src='/qr-code-pix.PNG' alt='qr-code-pix' style={{
+                        marginTop: 3,
+                        marginBottom: 3,
+                    }} />
                 </Flex>
                 <Text>
                     Ou, se preferir, pode usar a chave: <b>9860c0ad-1cfe-4b59-9d76-1a43a4e3461e</b>
